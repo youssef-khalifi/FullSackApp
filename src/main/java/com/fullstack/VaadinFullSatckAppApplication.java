9374406adb82cab7487f2a5d0fd469f7de4b0ff1
youssef khalifi <youssefkhalifi767@gmail.com>
2024-05-05 22:43:41 +0100

    create the app

---
+++ src/main/java/com/fullstack/VaadinFullSatckAppApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinFullSatckAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaadinFullSatckAppApplication.class, args);
+    }
+
+}
